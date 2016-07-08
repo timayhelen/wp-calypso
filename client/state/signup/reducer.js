8879b2fd4378bfb9192a20330bb09ccad901df4f
Biser Perchinkov <biser@bisko.be>
2016-07-08 18:50:35 +0300

    Suggested username is now saved in the Redux state as an optional dependency.

--- client/state/signup/reducer.js
+++ client/state/signup/reducer.js
@@ -7,7 +7,9 @@ import { combineReducers } from 'redux';
  * Internal dependencies
  */
 import dependencyStore from './dependency-store/reducers';
+import optionalDependencies from './optional-dependencies/reducers';
 
 export default combineReducers( {
 	dependencyStore,
+	optionalDependencies,
 } );
