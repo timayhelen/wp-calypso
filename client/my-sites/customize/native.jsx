c56fcc3504803ce0b4baa2e4bf3a0e1363441579
Payton Swick <payton@foolord.com>
2016-07-13 17:45:32 -0400

    Customize: activate the preview mode instead of rendering
    This makes use of the preview mode's DesignPreview component rather than
    creating our own.

--- client/my-sites/customize/native.jsx
+++ client/my-sites/customize/native.jsx
@@ -2,22 +2,37 @@
  * External Dependencies
  */
 import React from 'react';
+import { connect } from 'react-redux';
 
 /**
  * Internal Dependencies
  */
-import DesignMenu from 'my-sites/design-menu';
-import DesignPreview from 'my-sites/design-preview';
+import layoutFocus from 'lib/layout-focus';
+import { showPreviewSidebar, hidePreviewSidebar } from 'state/ui/actions';
 
 const NativeCustomizer = React.createClass( {
+	propTypes: {
+		showPreviewSidebar: React.PropTypes.func.isRequired,
+		hidePreviewSidebar: React.PropTypes.func.isRequired,
+	},
+
+	componentWillMount() {
+		this.props.showPreviewSidebar();
+		layoutFocus.set( 'preview' );
+	},
+
+	componentWillUnmount() {
+		this.props.hidePreviewSidebar();
+		// TODO: or should I use setNext? and do 'sidebar'?
+		layoutFocus.set( 'content' );
+	},
+
 	render() {
 		return (
 			<div className="customize__native-customizer">
-				<DesignMenu />
-				<DesignPreview showPreview={ true } showClose={ false } showExternal={ false } />
 			</div>
 		);
 	}
 } );
 
-export default NativeCustomizer;
+export default connect( () => ( {} ), { showPreviewSidebar, hidePreviewSidebar } )( NativeCustomizer );
