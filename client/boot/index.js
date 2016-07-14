4eef63ca6c9f84845e677f0bb60c528e0dafdbe3
Payton Swick <payton@foolord.com>
2016-07-14 13:44:06 -0400

    Boot: use setLayoutFocus for querystring focus
    Using `?sb` or `?sp` in the querystring should focus the sidebar or the sites
    list, respectively.

--- client/boot/index.js
+++ client/boot/index.js
@@ -54,6 +54,8 @@ var config = require( 'config' ),
 	// The following components require the i18n mixin, so must be required after i18n is initialized
 	Layout;
 
+import { setLayoutFocus } from 'state/ui/actions';
+
 function init() {
 	var i18nLocaleStringsObject = null;
 
@@ -235,7 +237,7 @@ function reduxStoreReady( reduxStore ) {
 	page( '*', function( context, next ) {
 		if ( [ 'sb', 'sp' ].indexOf( context.querystring ) !== -1 ) {
 			layoutSection = ( context.querystring === 'sb' ) ? 'sidebar' : 'sites';
-			layoutFocus.set( layoutSection );
+			reduxStore.dispatch( setLayoutFocus( layoutSection ) );
 			page.redirect( context.pathname );
 		}
 
