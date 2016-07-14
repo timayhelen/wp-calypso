d61b0ef610e31322a1473ad51da86eac83a4b508
Payton Swick <payton@foolord.com>
2016-07-14 18:07:56 -0400

    LayoutFocus: use redux actions and selectors
    Binds the layoutFocus module (Flux store) to the Redux store and uses it as the
    source of truth for the layoutFocus.

--- client/lib/layout-focus/index.js
+++ client/lib/layout-focus/index.js
@@ -3,9 +3,49 @@
 /**
  * Internal dependencies
  */
-var Emitter = require( 'lib/mixins/emitter' ),
-	config = require( 'config' );
+import Emitter from 'lib/mixins/emitter';
+import config from 'config';
+import { getCurrentLayoutFocus, getPreviousLayoutFocus } from 'state/ui/selectors';
+import { setLayoutFocus, setNextLayoutFocus, activateNextLayoutFocus } from 'state/ui/actions';
 
+// These are the structural areas
+// of the main body of Calypso
+const _areas = [ 'content', 'sidebar', 'sites', 'preview' ];
+function isValid( area ) {
+	const valid = _areas.indexOf( area ) !== -1;
+
+	if ( ! valid ) {
+		if ( config( 'env' ) === 'development' ) {
+			throw new Error( area + ' is not a valid layout focus area' );
+		}
+	}
+
+	return valid;
+}
+
+// This is unfortunately necessary to avoid layout elements still in the DOM
+// being targeted by things like browser inline search, which may cause odd
+// positioning effects by having their CSS modified.
+function setFocusHideClass() {
+	if ( typeof document === 'undefined' ) {
+		return;
+	}
+	// Whenever layout focus changes remove `focus-hide` so
+	// that animations can occur with all elements visible.
+	document.documentElement.classList.remove( 'focus-hide' );
+
+	// After transitions restore `focus-hide`
+	setTimeout( function() {
+		document.documentElement.classList.add( 'focus-hide' );
+	}, 200 );
+}
+
+// Used to keep a reference to the Redux store so we can update it when the
+// legacy Flux store is updated.
+let reduxStore;
+function bindLayoutFocusStore( store ) {
+	reduxStore = store;
+}
 
 /**
  * This module stores the current and previous
@@ -14,8 +54,11 @@ var Emitter = require( 'lib/mixins/emitter' ),
  *
  * These focus area values are whitelisted and used for informing
  * what the focus for any view of Calypso should be.
+ *
+ * This is a legacy Flux store; there is a Redux store available instead under
+ * state.ui.layoutFocus.
  */
-var layoutFocus = {
+const layoutFocus = {
 
 	// Store `current` and `previous` states
 	// as internal attributes
@@ -23,46 +66,49 @@ var layoutFocus = {
 	_previous: null,
 	_next: null,
 
-	// These are the three structural areas
-	// of the main body of Calypso
-	_areas: [ 'content', 'sidebar', 'sites', 'preview' ],
-
 	getCurrent: function() {
+		if ( reduxStore ) {
+			return getCurrentLayoutFocus( reduxStore.getState() );
+		}
 		return this._current || 'content';
 	},
 
 	getPrevious: function() {
+		if ( reduxStore ) {
+			return getPreviousLayoutFocus( reduxStore.getState() );
+		}
 		return this._previous;
 	},
 
 	set: function( area ) {
+		if ( reduxStore ) {
+			return reduxStore.dispatch( setLayoutFocus( area ) );
+		}
 
-		if ( ! this.isValid( area ) || area === this._current ) {
+		if ( ! isValid( area ) || area === this._current ) {
 			return;
 		}
 
 		this._previous = this._current;
 
-		this.setFocusHideClass();
-
+		setFocusHideClass();
 
 		// Update current state and emit change event
 		this._current = area;
 		this.emit( 'change' );
 	},
 
-	/**
-	 * Advanced to the next focus area if there is
-	 * one queued.
-	 */
 	next: function() {
-		var area = this._next;
+		if ( reduxStore ) {
+			return reduxStore.dispatch( activateNextLayoutFocus() );
+		}
+		let area = this._next;
 
 		// If we don't have a change queued and the focus has changed
 		// previously, set it to `content`. This avoids having to set the
 		// focus to content on all navigation links because it becomes the
 		// default after focus has shifted.
-		if ( ! area && this.hasChanged() ) {
+		if ( ! area && this._previous !== null ) {
 			area = 'content';
 		}
 
@@ -75,47 +121,18 @@ var layoutFocus = {
 		this.set( area );
 	},
 
-	hasChanged: function() {
-		return this._previous !== null;
-	},
-
-	isValid: function( area ) {
-		var valid = this._areas.indexOf( area ) !== -1;
-
-		if ( ! valid ) {
-			if ( config( 'env' ) === 'development' ) {
-				throw new Error( area + ' is not a valid layout focus area' );
-			}
+	setNext: function( area ) {
+		if ( reduxStore ) {
+			return reduxStore.dispatch( setNextLayoutFocus( area ) );
 		}
 
-		return valid;
-	},
-
-	setNext: function( area ) {
-
-		if ( ! this.isValid( area ) ) {
+		if ( ! isValid( area ) ) {
 			return;
 		}
 
 		this._next = area;
 	},
 
-	// This is unfortunately necessary to avoid layout elements still in the DOM
-	// being targeted by things like browser inline search, which may cause odd
-	// positioning effects by having their CSS modified.
-	setFocusHideClass: function() {
-		if ( typeof document === 'undefined' ) {
-			return;
-		}
-		// Whenever layout focus changes remove `focus-hide` so
-		// that animations can occur with all elements visible.
-		document.documentElement.classList.remove( 'focus-hide' );
-
-		// After transitions restore `focus-hide`
-		setTimeout( function() {
-			document.documentElement.classList.add( 'focus-hide' );
-		}, 200 );
-	}
 };
 
 /**
@@ -123,7 +140,7 @@ var layoutFocus = {
  */
 Emitter( layoutFocus );
 
-/**
- * Expose `laoutFocus` singleton
- */
+layoutFocus.bindLayoutFocusStore = bindLayoutFocusStore;
+layoutFocus.setFocusHideClass = setFocusHideClass;
+
 module.exports = layoutFocus;
