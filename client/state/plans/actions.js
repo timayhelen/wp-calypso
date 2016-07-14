d06b10c56e2f22139cd5c01ce32f008f585662d1
Rodrigo Iloro <rodrigo.iloro@automattic.com>
2016-07-14 14:04:58 -0300

    Removes hardcoded data for the Personal Plan

--- client/state/plans/actions.js
+++ client/state/plans/actions.js
@@ -9,7 +9,6 @@ import {
 	PLANS_REQUEST_FAILURE
 } from '../action-types';
 
-import { insertPersonalPlan } from 'lib/plans/personal-plan';
 /**
  * Action creator function: RECEIVE
  *
@@ -19,7 +18,7 @@ import { insertPersonalPlan } from 'lib/plans/personal-plan';
 export const plansReceiveAction = plans => {
 	return {
 		type: PLANS_RECEIVE,
-		plans: insertPersonalPlan( plans )
+		plans
 	};
 };
 
@@ -81,7 +80,7 @@ export const requestPlans = () => {
 
 		return wpcom
 			.plans()
-			.list( { apiVersion: '1.2' } )
+			.list( { apiVersion: '1.4' } )
 			.then( data => {
 				dispatch( plansRequestSuccessAction() );
 				dispatch( plansReceiveAction( getValidDataFromResponse( data ) ) );
